6f1775320fcf785bc0a39a3cc000f54a1ed0ecb1
PW <pangnima@gmail.com>
2018-09-15 16:17:34 +0900

    첫커밋

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+  hero : Hero = {
+    id:1,
+    name:'Winstorm'
+  }
+  isSpecial = true;
+
+  heroes = HEROES;
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+  onSave(e:any){
+    console.log(e);
+    this.isSpecial = !this.isSpecial;
+  }
+
+}
